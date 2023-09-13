edf80d6285f2b0ff4551df9d1a457eb63837609a
JsFrancisc0 <j.saldias03@ufromail.cl>
2023-09-13 10:11:54 -0300

    metodos geometria

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -43,4 +43,76 @@ return 3;
     }public static double division (double n1, double n2){
         return n1/n2;
     }
+
+    public static double areaRectangulo(double base, double altura){
+
+        double area = base*altura;
+
+        return area;
+
+    }
+
+    public static double perimetroRectangulo(double base, double altura){
+
+        double perimetro = base*2+altura*2;
+
+        return perimetro;
+
+    }
+
+    public static double perimetroCuadrado(double lado){
+
+        double perimetro = lado*4;
+
+        return perimetro;
+
+    }
+
+    public static double areaCuadrado(double lado){
+
+        double area = lado*2;
+
+        return area;
+
+    }
+
+    public static double areaCirculo(double radio){
+
+        double area = Math.PI * radio *radio;
+
+        return area;
+
+    }
+
+    public static double perimetroCirculo(double radio){
+
+        double perimetro = 2* Math.PI * radio;
+
+        return perimetro;
+
+    }
+
+    public static double volumenEsfera(double radio){
+
+        double volumen = 4/3 * Math.PI * radio * radio * radio;
+
+        return volumen;
+
+    }
+
+    public static double volumenCono(double radio, double altura){
+
+        double volumen = 1/3 * areaCirculo(radio) * altura;
+
+        return volumen;
+
+    }
+
+    public static double volumenCubo(double lado){
+
+        double volumen = areaCuadrado(lado) * lado;
+
+        return volumen;
+
+    }
 }
